1dd241e88b16653bce9d18fecf5b9e4f05e06ffe
Leo Wu <leo.wu@goanimate.com>
2017-03-11 18:41:40 +0800

    Currency API OK

--- server/src/DB.js
+++ server/src/DB.js
@@ -1,4 +1,5 @@
 var config  = require('config');
+var Promise  = require('bluebird');
 var admin = require("firebase-admin");
 var serviceAccount = require(config.firebaseJsonPath);
 
@@ -10,9 +11,21 @@ admin.initializeApp({
 var database = admin.database();
 
 class DB {
+
 	static set(directory, key, value) {
-		admin.database().ref(directory).set({ [key]: value });
+		database.ref(`${directory}/${key}`).set(value);
 	}
+
+	static get(directory, key) {
+		return new Promise((resolve, reject) => {
+			var ref  = database.ref(`${directory}/${key}`);
+
+			ref.on('value', function(snapshot) {
+				resolve(snapshot.val());
+			});
+		});
+	}
+
 }
 
 module.exports = DB;
